b6a35d9c7c7f30c1e2758143c6d122e134511f3b
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-09-08 00:44:14 +0200

    1.3

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const books = require('./routes/books');
-const auth = require('./routes/user');
+//const auth = require('./routes/user');
+const user = require("./controllers/User");
 const path = require('path');
 
 app.use(express.json()); 
 // Activer CORS avec le middleware cors
-app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -23,14 +23,9 @@ mongoose.connect('mongodb+srv://newuser:MongoDB@cluster0.v4ebrt9.mongodb.net/?re
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-
-  app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-  });
-
   // Ajout des routes pour livres, authentification utilisateur et images statiques
   app.use('/api/books', books)
-  app.use('/api/auth', auth)
+  app.use('/api/auth', user)
   app.use('/images', express.static(path.join(__dirname, 'images')));
   
 
